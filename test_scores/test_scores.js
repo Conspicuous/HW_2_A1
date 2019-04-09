949bd18dd3de635acca5d05d493762d232feb9cb
Olts <snowstormdorn@gmail.com>
2019-04-09 17:19:05 -0400

    A2

--- test_scores/test_scores.js
+++ test_scores/test_scores.js
@@ -1,12 +1,16 @@
-var names = ["Ben", "Joel", "Judy", "Anne"];
-var scores = [88, 98, 77, 88];
+var names = ["Ben", "Joel", "Judy", "Anne"]; //Names Array CD, global
+var scores = [88, 98, 77, 88]; //Scores Array CD, global
 var textDisplay;
 
-var $ = function (id) { return document.getElementById(id); }
+
+/* NOTE This syntax utilizes functional programming concepts w/ some object oriented text.
+
+Note that a pure functiuon, given the same inputs, will always return the same outputs, with no side effects.  */
+var $ = function (id) { return document.getElementById(id); };
 
 var addElement = function () {
 	// get user entries
-	var name = $("name").value;
+	var name = $("name").value; //local
     var score  = parseInt( $("score").value );
     
     // check entries for validity
@@ -14,16 +18,57 @@ var addElement = function () {
     	alert("You must enter a name and a valid score");
     }
 	else {
-		names[names.length] = $("name").value;
+		names[names.length] = $("name").value; // syntax pushes
 		scores[scores.length] = parseInt($("score").value);
 	    $("name").value = "";
 	    $("score").value = "";
+	    $("results").value=""; //clear results on entry of new name
 	}
     $("name").focus();
-}
+};
+
+var determineBest=function(){
+	// const high_score=([x,...xs], result=0)=> def(x)
+	// ? x > result
+	// 	? max(xs,x)
+	// 		:max(xs,result)
+	// 	: result
+
+	let Max = Math.max(...scores); //ES6 includes the 3 dots.
+	let Name=names[scores.indexOf(Max)]; //Name of high score = the index of the value of max score.
+
+
+	$("results").value="High Score Student: "+Name+"\n"+"High Score = "+ Max;
+
+
+
+
+};
+
+var listData=function(){
+	for(let i=0;i<names.length;i++){
+		text+=names[i]+", "+scores[i]+"\n";
+
+	}
+
+	$("results").value=text;
+};
+
+
+
+let i=0;
+while(i<names.length){
+
+
+	i++
+};
+Text=
+
 window.onload = function () {
 	$("add").onclick = addElement;
 	$("name").focus();
-}
+	$("show_best").onclick=determineBest;
+	$("list_array").onclick=listData;
+};
 
 
